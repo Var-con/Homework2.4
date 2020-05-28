2fd6128ff655a7d33055099354a8a221805f6859
Stanislav Klimov <kpuctallaz@gmail.com>
2020-05-29 00:37:01 +0500

    Initial Commit

---
+++ Homework2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2.4
+//
+//  Created by Станислав Климов on 29.05.2020.
+//  Copyright © 2020 Stanislav Klimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
